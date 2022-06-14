4729c8f429fe1b69b5d5f5ae8697f21b79a78152
Travis <tzito93@gmail.com>
2022-06-14 16:20:56 -0500

    added a sample user to the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, length: { minimum: 3 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 40 }
+  validates :password, presence: true, length: { maximum: 40 }
 end
