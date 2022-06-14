ddaae848e2155710e73d11346d3f78c48490f415
Travis <tzito93@gmail.com>
2022-06-14 17:15:49 -0500

    migrated comments to db with basic comment validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :name, length: { minimum: 3 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 40 }
