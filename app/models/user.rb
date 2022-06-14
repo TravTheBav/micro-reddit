70020a3bbfe82cc23d4c68665455bcc010a80f09
Travis <tzito93@gmail.com>
2022-06-14 16:59:30 -0500

    added in association between Posts and Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, length: { minimum: 3 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 40 }
   validates :password, presence: true, length: { maximum: 40 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_14_205034) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_214531) do
+  create_table "posts", force: :cascade do |t|
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_14_205034) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
